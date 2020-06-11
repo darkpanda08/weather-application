5198abc6d42deb0bd75fb605ad5fe3000ca5d612
Vineet Ranjan <vineetranjan65@gmail.com>
2020-06-11 11:06:11 +0530

    Initital Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+var morgan = require('morgan')
+
+const app = express();
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Morgan
+app.use(morgan('combined'))
+
+// Bodyparser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/public', express.static('public'));
+
+// Routes
+app.use('/', require('./routes/index'));
+
+// 404
+app.use((req, res, next) => {
+    res.status(404);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT , console.log(`Server started on port ${PORT}`));
